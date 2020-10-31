5f5596263de83348a05b6216f861cbdc7734796b
Miguel <miguel@ganesha>
2020-10-31 15:39:23 -0300

    refactor to avoid duplications

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,76 +1,72 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-  DB = mongoose.model('DBmodel');
+    DB = mongoose.model('DBmodel');
 
 // retorna todas as receitas
 exports.list_all = function(req, res) {
-  DB.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+    DB.find({}, function(err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
 };
 
 // sanitiza os campos
 function sanitize(body){
-  for(let i = 0; i< body.ingredientes.length;i++){
-    body.ingredientes[i].qtde = eval(body.ingredientes[i].qtde // avalia a fração/soma
-                                     .replace(/ +/,'+')   // troca 1 ou mais espaços por '+'
-                                     .replace(/^\+/,'')   // remove '+' no início
-                                     .replace(/\+$/,'')); // remove '+' no fim
-  }
+    for(let i = 0; i< body.ingredientes.length;i++){
+        body.ingredientes[i].qtde = eval(body.ingredientes[i].qtde // avalia a fração/soma
+                                         .replace(/ +/,'+')   // troca 1 ou mais espaços por '+'
+                                         .replace(/^\+/,'')   // remove '+' no início
+                                         .replace(/\+$/,'')); // remove '+' no fim
+    }
+}
+
+function handleResponse(res, err, task) {
+    if (err){
+        console.log(err);
+        res.send(err);
+    }
+    res.json(task);
 }
 
 // cria a receita recebida em req.body
 exports.create = function(req, res) {
-  sanitize(req.body);
-  var new_task = new DB(req.body);
-  new_task.save(function(err, task) {
-    if (err){
-      console.log(err);
-      res.send(err);
-    }
-    res.json(task);
-  });
+    sanitize(req.body);
+    var new_task = new DB(req.body);
+    new_task.save(handleResponse.bind(null, res));
 };
 
 // retorna a receita com id = :id
 exports.get_by_id = function(req, res) {
-  DB.findById(req.params.id, function(err, task) {
-    if (err){
-      console.log(err);
-      res.send(err);
-    }
-    res.json(task);
-  });
+    DB.findById(req.params.id, handleResponse.bind(null, res));
 };
 
 // atualiza a receita com id = :id
 // se a receita nao existe, cria
 // retorna a receita atualizada
 exports.update_by_id = function(req, res) {
-  sanitize(req.body);  
-  DB.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, task) {
-    if (err){
-      console.log(err);
-      res.json({ message: 'Item com id: '+req.params.id+' não foi encontrado'});
-    }
-    res.json(task);
-  });
+    sanitize(req.body);  
+    DB.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, task) {
+        if (err){
+            console.log(err);
+            res.json({ message: 'Item com id: '+req.params.id+' não foi encontrado'});
+        }
+        res.json(task);
+    });
 };
 
 // remove a receita com id = :id
 exports.delete_by_id = function(req, res) {
-  DB.remove({
-    _id: req.params.id
-  }, function(err, task) {
-    if (err){
-      console.log(err);
-      res.send(err);
-    }
-    res.json({ message: 'Item removido com sucesso (id: '+req.params.id+')'});
-  });
+    DB.remove({
+        _id: req.params.id
+    }, function(err, task) {
+        if (err){
+            console.log(err);
+            res.send(err);
+        }
+        res.json({ message: 'Item removido com sucesso (id: '+req.params.id+')'});
+    });
 };
 
 // recebe { 'string': string,
@@ -78,19 +74,19 @@ exports.delete_by_id = function(req, res) {
 //          'regex' : boolean }
 // retorna todos os itens que satisfizerem a busca
 exports.search = function(req, res) {
-  let str = req.body.string;
-  let campo = req.body.campo;
-  let regex = req.body.regex || false;
-  let query = {[campo]:str};
-  if(regex){
-    query = {[campo]:{$regex:str}};
-  }
-  var cursor = DB.find(query).cursor(); 
-  var r = []
-  cursor.on('data', function(doc){
-    r.push(doc);
-  });
-  cursor.on('close', function(){
-    res.send(r);
-  });
+    let str = req.body.string;
+    let campo = req.body.campo;
+    let regex = req.body.regex || false;
+    let query = {[campo]:str};
+    if(regex){
+        query = {[campo]:{$regex:str}};
+    }
+    var cursor = DB.find(query).cursor(); 
+    var r = []
+    cursor.on('data', function(doc){
+        r.push(doc);
+    });
+    cursor.on('close', function(){
+        res.send(r);
+    });
 }
